68d4c8286edc655f8ec7b64a5eb090c46eee07b3
vois-asakry <94744689+vois-asakry@users.noreply.github.com>
2023-05-31 12:35:26 +0300

    Add files via upload

---
+++ lib/screens/custom/static_site.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:inspireui/inspireui.dart' show PlatformError;
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../../common/constants.dart';
+import '../common/app_bar_mixin.dart';
+
+class StaticSite extends StatefulWidget {
+  final String? data;
+
+  const StaticSite({this.data});
+
+  @override
+  State<StaticSite> createState() => _StaticSiteState();
+}
+
+class _StaticSiteState extends State<StaticSite> with AppBarMixin {
+  @override
+  Widget build(BuildContext context) {
+    var controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadHtmlString('data:text/html;base64,${widget.data}');
+
+    return renderScaffold(
+      routeName: RouteList.html,
+      child: !isMobile
+          ? const PlatformError()
+          : SafeArea(
+              child: WebViewWidget(controller: controller),
+            ),
+    );
+  }
+}
